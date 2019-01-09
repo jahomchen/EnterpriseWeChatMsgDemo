9f2022672455e983f88a91cfb12a7f2deeefb1e9
chenjiahong <chenjiahong@beisen.com>
2019-01-09 17:39:18 +0800

    添加项目

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using EnterpriseWeChatMsgDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var templateMsg = "测评中心邀请信息<br/><br/>你好，<br/>请于"
+				+ DateTime.Now.AddDays(5) +
+				"之前完成测试,请点击www.baidu.com开始答题。";
+
+			EnterpriseWeChatHelper.SendText("OuDaAoTuManDeXiaoGuaiShou", templateMsg);
+
+			Console.ReadKey();
+		}
+	}
+}
